0911ace03b89ecbf9cc4ff99bacfde6eb7800b8c
iansummerlin <isummerlin95@gmail.com>
2019-09-06 08:38:39 +0100

    Added mute button functionality

--- includes/nowPlayingContainer.php
+++ includes/nowPlayingContainer.php
@@ -90,6 +90,12 @@
         $(".controlButton.repeat img").attr("src", "assets/images/icons/" + imageName);
     }
 
+    function setMute() {
+        audioElement.audio.muted = !audioElement.audio.muted;
+        var imageName = audioElement.audio.muted ? "volume-mute.png" : "volume.png";
+        $(".controlButton.volume img").attr("src", "assets/images/icons/" + imageName);
+    }
+
     function setTrack(trackId, newPlaylist, play) {
 
         currentIndex = currentPlaylist.indexOf(trackId);
@@ -191,7 +197,7 @@
 
         <div id="nowPlayingRight">
             <div class="volumeBar">
-                <button class="controlButton volume" title="Volume button">
+                <button class="controlButton volume" title="Volume button" onclick="setMute()">
                     <img src="assets/images/icons/volume.png" alt="Volume button">
                 </button>
                 <div class="progressBar">
