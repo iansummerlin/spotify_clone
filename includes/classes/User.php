aba3b7e1f1825b022c6d99487a81ae453dc2deb8
iansummerlin <isummerlin95@gmail.com>
2019-09-10 20:56:58 +0100

    Renamed playlist.php to myMusic.php and added User class

---
+++ includes/classes/User.php
@@ -0,0 +1,15 @@
+<?php 
+    class User {
+        private $con;
+        private $username;
+
+        public function __construct($con, $username) {
+            $this->con = $con;
+            $this->username = $username;
+        }
+
+        public function getUsername() {
+            return $this->username;
+        }
+    }
+?>
